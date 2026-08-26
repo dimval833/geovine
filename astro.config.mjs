b61832fabbd22f315ded38faa56818923dc9b18f
Δήμητρα Βαλαβάνη <dimval833@gmail.com>
2026-08-26 18:34:52 +0300

    andagain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  // Εδώ βάζεις το νέο σου domain
   site: 'https://www.geovine.gr',
   
+  // Το base πρέπει να γίνει σκέτη κάθετος (ή να διαγραφεί τελείως η γραμμή)
   base: '/',
 });
