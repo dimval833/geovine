a6e0b311725398f04e277222102c3ecfdafee594
Δήμητρα Βαλαβάνη <dimval833@gmail.com>
2026-08-26 18:21:17 +0300

    tstt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://dimval833.github.io',
-  base: '/geovine',
+  site: 'https://www.geovine.gr',
+  
+  base: '/',
 });
